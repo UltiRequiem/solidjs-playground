d2f96fb05994b649843b856e24b95d1269967450
Eliaz Bobadilla <eliaz.bobadilladev@gmail.com>
2022-01-29 22:09:34 -0500

    :sparkles: Introduction/Effects

--- tutorial/src/App.tsx
+++ tutorial/src/App.tsx
@@ -1,26 +1,37 @@
-import type { Component } from 'solid-js';
+import { createSignal, createEffect } from "solid-js";
 
-import logo from './logo.svg';
-import styles from './App.module.css';
+import type { Component } from "solid-js";
 
 const App: Component = () => {
+  const name = "Zero";
+  const [count, setCount] = createSignal(0);
+
+  createEffect(() => {
+    console.log(`The count is now ${count()}!`);
+  });
+
+  const svg = (
+    <svg height="300" width="400">
+      <defs>
+        <linearGradient id="gr1" x1="0%" y1="60%" x2="100%" y2="0%">
+          <stop offset="5%" style="stop-color:rgb(255,255,3);stop-opacity:1" />
+          <stop offset="100%" style="stop-color:rgb(255,0,0);stop-opacity:1" />
+        </linearGradient>
+      </defs>
+      <ellipse cx="125" cy="150" rx="100" ry="60" fill="url(#gr1)" />
+      Sorry but this browser does not support inline SVG.
+    </svg>
+  );
+
+  setInterval(() => setCount((c) => c + 1), 1000);
+
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+      <p> Hello {name}!</p>
+      {svg}
+      {count()}
+      <button onClick={() => setCount(count() + 1)}>Click Me</button>
+    </>
   );
 };
 
